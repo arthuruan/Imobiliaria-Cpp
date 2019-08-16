c0b5f7d04c32af8d3a9abb1d06c198b67b711d33
Arthur <arthur123ruan@hotmail.com>
2019-08-16 11:49:13 -0300

    Arquivo menu implementado

--- Menu.h
+++ Menu.h
@@ -1,10 +1,23 @@
 #ifndef MENU_H
 #define MENU_H
 
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#define CLEAR system("cls")
+#define SETA_BAIXO 80
+#define SETA_CIMA 72
+#define ENTER 13
+
 class Menu{
     public:
         int Menu1();
-
+        int Menu2();
+        int Menu3();
+        int Menu4();
+        int Menu5();
+        int Menu6();
+        int Menu7();
 };
 
 #endif // MENU_H
